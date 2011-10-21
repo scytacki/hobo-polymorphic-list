c91a8bb618d64dbd6e1b83e50825f18c729142f6
Scott Cytacki <scytacki@concord.org>
2011-10-21 09:24:12 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoboPolymorphicList::Application.config.secret_token = '5ceed28dd9b1a26dbf2d43997f4ed6b6d8cbb14f2f276b519157167eb05c92e629a5438d9951cb22687a7bf51aa83a2f9e7cf2ebd5b0649c64a9c01927a62e96'
